9018bdcfb894afbd3fa8e1dc294928ca96603e5d
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-02-25 13:01:12 -0600

    Structured calculator: added main container and divs for display and button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,6 +2,10 @@ import React from "react";
 
 const Calculator = () => {
     return (
+        <div id="calculator">
+            <div id="display">display</div>
+            <div id="buttons">buttons</div>
+        </div>
     );
 };
 
