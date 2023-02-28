c7c9da3a65f9b7887b35a249534a3909dab4fb77
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-02-27 18:05:13 -0600

    Added states for display and handleClick for buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
+const buttonData = {
+    "clear": "CLEAR",
+    "divide": "/",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+    "multiply": "x",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "subtract": "-",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "add": "+",
+    "zero": "0",
+    "decimal": ".",
+    "equals": "="  
+}
+
 const Calculator = () => {
-    const buttonData = {
-        "clear": "CLEAR",
-        "divide": "/",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9",
-        "multiply": "*",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "subtract": "-",
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "add": "+",
-        "zero": "0",
-        "decimal": ".",
-        "equals": "="  
-    }
+    const [input, setInput] = useState('');
+    const [output, setOutput] = useState('0');
+
+    const handleClick = (buttonSymbol) => {
+        setInput(input + buttonSymbol);
+    };
 
     return (
         <div id="calculator">
-            <div id="display">display</div>
+            <div id="display">
+                {input}
+                <p id="output">{output}</p>
+            </div>
             <div id="buttons">
                 {Object.keys(buttonData).map(buttonName => 
-                        <Button buttonName={buttonName} key={buttonName} buttonKey={buttonData[buttonName]} />
+                    <Button buttonName={buttonName} key={buttonName} buttonSymbol={buttonData[buttonName]} handleClick={handleClick} />
                 )}
             </div>
         </div>
